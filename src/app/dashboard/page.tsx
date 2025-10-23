f0a69a2be3d1bbfc3fb817fb24994f286ad69a4f
Du Plessis, Brendin <brendin.duplessis@affiliate.imd.org>
2025-10-23 15:28:40 +0200

    auth functional

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://ktctqojjjdxwizztkkmc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0Y3Rxb2pqamR4d2l6enRra21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDQyMjYsImV4cCI6MjA3MjM4MDIyNn0.obILD95-ZimwoI-CQlaXDN2QRr0fInbki1AOWa47O0M"
+);
+
 export default function DashboardPage() {
-  return <h1 className="text-xl font-semibold">Welcome to your Dashboard</h1>;
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      setUser(user);
+      
+      if (user) {
+        const { data: profile } = await supabase
+          .from('profiles')
+          .select('display_name')
+          .eq('id', user.id)
+          .single();
+        setProfile(profile);
+      }
+    };
+    getUser();
+  }, []);
+
+  return <h1 className="text-xl font-semibold">Welcome to your Dashboard,   
+  {profile?.display_name}</h1>;
 }
